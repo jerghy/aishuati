0f76f33dbc394af995a1c989965a30131979affc
jerghy <113653561+jerghy@users.noreply.github.com>
2025-03-15 22:27:29 +0800

    Update mod.py

--- mod.py
+++ mod.py
@@ -2,10 +2,12 @@ from openai import OpenAI
 
 api_openai="https://api.gptapi.us/v1/chat/completions"
 api_anthropic="https://api.gptapi.us/v1/messages"
-free_key="sk-htxjzlywpzxdjkczbmsazayluvhoilxwupboyfdzixgckwsp", # 从https://cloud.siliconflow.cn/account/ak获取
+free_key="sk-ethgdjypryiwfidbmcqxpzatzsfvjgrcjpdkqsvsbubusauk", # 从https://cloud.siliconflow.cn/account/ak获取
 free_url="https://api.siliconflow.cn/v1/chat/completions"
-
-
+doubaourl="https://ark.cn-beijing.volces.com/api/v3/chat/completions"
+doubaokey="0f38123d-549a-48c5-a2ab-46dde690c019"
+aliyunurl="https://dashscope.aliyuncs.com/compatible-mode/v1"
+aliyunkey="sk-f104fdb232104364b72b565df9839a82"
 class gptModel:
     def __init__(self):
         self.o3_mini="o3-mini"
@@ -29,10 +31,21 @@ class nofreemod:
     def __init__(self):
         # Qwen2-VL-7B-Instruct (Pro)
         self.Qwen2_VL_7B_Instruct="Pro/Qwen/Qwen2-VL-7B-Instruct"
-
+class doubaomod:
+    def __init__(self):
+        # Doubao-1.5-vision-pro-32k
+        self.Doubao_1_5_vision_pro_32k="doubao-1-5-vision-pro-32k-250115"
+        # Doubao-vision-lite-32k
+        self.Doubao_vision_lite_32k="doubao-vision-lite-32k-241015"
+class aliModel:
+    def __init__(self):
+        # qwen2.5-vl-7b-instruct
+        self.qwen2_5_vl_7b_instruct="qwen2.5-vl-7b-instruct"
 
 gptmods=gptModel()
 freemods=freeModel()
 nofreemods=nofreemod()
+doubaomods=doubaomod()
+aliModels=aliModel()
 
 api_key="sk-L2rYdR6vAoFYIrwDF8B863D5274a4fFd85012c54EcC93424"
